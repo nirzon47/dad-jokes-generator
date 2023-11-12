b9381e4f5cf1e85c1a12d8209f98bb5165955bfc
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-12 22:47:35 +0530

    UI v0

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['*.html'],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: [
+			{
+				custom: {
+					primary: '#39B6A9',
+
+					secondary: '#D48EC0',
+
+					accent: '#96B1AC',
+
+					neutral: '#334B48',
+
+					'base-100': '#292524',
+
+					info: '#9C5B8A',
+
+					success: '#29a387',
+
+					warning: '#c09a11',
+
+					error: '#ec3f3c',
+				},
+			},
+		],
+	},
+	plugins: [require('daisyui'), require('tailwindcss-animated')],
+}
